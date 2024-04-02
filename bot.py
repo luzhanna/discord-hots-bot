c0ff0f7ae338988ab759aa0689a13e79250ebe56
Liza <lizakurilina@gmail.com>
2024-04-02 14:50:25 +0400

    initial

---
+++ bot.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+from utils import *
+from token import *
+
+hero_desc = '!hero - генерация рандомного героя с рандомным билдом'
+build_desc = '!build - генерация билда для указанного после команды героя. ' + 'Героя писать на английском, хотя бы первые 5 букв. некоторые общепринятые сокращиения принимаются. ' + '(Например для билда на Келтузада можно ввести "kelthuzad" или "Kel" или "ktz") ' + 'Например(билд на самуро): !build sam'
+role_desc = '!role - генерация героя с указанной ролью(ролями) после команды. ' + 'Роли указываются одной буквой через запятую: t - tank, b - bruiser, m - melee, r - ranged, h - heal, s - support. ' + 'Например(сгенерить рендж дд или хила): !role r,h'
+
+print('started')
+
+with open('heroes.json', 'r', encoding="utf-8") as fd:
+	data = eval(fd.read())
+
+print('data had read')
+
+intents = discord.Intents(messages=True, guilds=True)
+intents.message_content = True
+
+bot = commands.Bot(intents=intents, command_prefix='!')
+
+print('bot made')
+
+@bot.event
+async def on_ready():
+	print(f'Logged in as {bot.user.name}  ({bot.user.id})')
+
+@bot.command()
+async def hero(ctx, brief='random hero with random build', descrintion=hero_desc):
+	output = random_hero(data)
+	await ctx.send(output)
+
+@bot.command()
+async def build(ctx, arg, brief='build for specific hero', descrintion=build_desc):
+	output = hero_build(arg, data)
+	await ctx.send(output)
+
+@bot.command()
+async def role(ctx, arg, brief='random hero+build of specific role', descrintion=role_desc):
+	output = roled_hero(arg, data)
+	await ctx.send(output)
+
+bot.run(TOKEN)	
---
+++ utils.py
@@ -0,0 +1,84 @@
+import random
+
+
+def take_random_build(hero):
+
+	talents = hero['talents']
+	build = []
+
+	for i in range(7):
+		num = random.randint(1,talents[i])
+		build.append(num)
+
+	if hero['ult'] == 'c':
+		while build[3] + build[6] == 3:
+			build[6] = random.randint(1,talents[6])
+	else:
+		if hero['name'] == 'varian':
+			while build[6] not in [build[1], 4, 5]:
+				build[6] = random.randint(1,talents[6])
+
+	return build
+
+
+def take_random_hero(lst):
+	num = random.randint(0, len(lst)-1)
+	return lst[num]
+
+
+def take_hero(hero_name, lst):
+
+	name = hero_name.strip().lower()
+	name = ''.join([i for i in name if i.isalpha()])
+
+	for hero in lst:
+		if name.startswith(hero['alts']):
+			return hero
+		elif hero['name'].startswith(name[:4]):
+			return hero
+
+	return ValueError
+
+
+def take_heroes_of_role(string, lst):
+
+	roles = string.strip().lower().split(',')
+	new_list = []
+
+	for hero in lst:
+		if hero['role'] in roles:
+			new_list.append(hero)
+
+	return new_list
+
+
+def finalize_output(build, hero):
+	output = '[' + 'T' + ''.join(map(str,build)) + ',' + hero['name'] + ']'
+	return output
+
+
+'''MAIN FUNCTIONS'''
+
+def random_hero(lst):
+
+	hero = take_random_hero(lst)
+	build = take_random_build(hero)
+
+	return finalize_output(build, hero)
+
+
+def roled_hero(roles, lst):
+
+	heroes = take_heroes_of_role(roles, lst)
+	hero = take_random_hero(heroes)
+	build = take_random_build(hero)
+
+	return finalize_output(build, hero)
+
+
+def hero_build(hero_name, lst):
+
+	hero = take_hero(hero_name, lst)
+	build = take_random_build(hero)
+
+	return finalize_output(build, hero)
