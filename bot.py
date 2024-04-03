946b92e167030a582c7a876ba9036c9548227543
Liza <lizakurilina@gmail.com>
2024-04-03 22:46:00 +0400

    changes in token name

--- bot.py
+++ bot.py
@@ -1,25 +1,26 @@
 import discord
 from discord.ext import commands
 from utils import *
-from token import *
+from my_token import MY_TOKEN
+
 
 hero_desc = '!hero - генерация рандомного героя с рандомным билдом'
-build_desc = '!build - генерация билда для указанного после команды героя. ' + 'Героя писать на английском, хотя бы первые 5 букв. некоторые общепринятые сокращиения принимаются. ' + '(Например для билда на Келтузада можно ввести "kelthuzad" или "Kel" или "ktz") ' + 'Например(билд на самуро): !build sam'
-role_desc = '!role - генерация героя с указанной ролью(ролями) после команды. ' + 'Роли указываются одной буквой через запятую: t - tank, b - bruiser, m - melee, r - ranged, h - heal, s - support. ' + 'Например(сгенерить рендж дд или хила): !role r,h'
+build_desc = ('!build - генерация билда для указанного после команды героя. '  
+	'Героя писать на английском, хотя бы первые 5 букв. некоторые общепринятые сокращиения принимаются. '
+	'(Например для билда на Келтузада можно ввести "kelthuzad" или "Kel" или "ktz") '
+	'Например(билд на самуро): !build sam')
+role_desc = ('!role - генерация героя с указанной ролью(ролями) после команды. '
+	'Роли указываются одной буквой через запятую: t - tank, b - bruiser, m - melee, r - ranged, h - heal, s - support. '
+	'Например(сгенерить рендж дд или хила): !role r,h')
 
-print('started')
 
 with open('heroes.json', 'r', encoding="utf-8") as fd:
 	data = eval(fd.read())
-
-print('data had read')
 
 intents = discord.Intents(messages=True, guilds=True)
 intents.message_content = True
 
 bot = commands.Bot(intents=intents, command_prefix='!')
-
-print('bot made')
 
 @bot.event
 async def on_ready():
@@ -40,4 +41,4 @@ async def role(ctx, arg, brief='random hero+build of specific role', descrintion
 	output = roled_hero(arg, data)
 	await ctx.send(output)
 
-bot.run(TOKEN)	
+bot.run(MY_TOKEN)	
